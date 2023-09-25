7dd5216322eaaed1271a5bdf1e583b3d08fd1da4
Lars Ivar Hatledal <laht@ntnu.no>
2023-09-25 13:23:37 +0200

    formatting

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -4,24 +4,24 @@
 #include <fstream>
 
 std::optional<std::string> FileReader::read(const std::filesystem::path &path) {
-    
+
     if (cache_.contains(path.string())) {
         return cache_[path.string()];
     }
-    
+
     std::ifstream file(path);
-    
+
     if (file.is_open()) {
-        
+
         std::string content;
         std::string line;
-        while(std::getline(file, line)) {
+        while (std::getline(file, line)) {
             content += line + "\n";
         }
-        
+
         cache_[path.string()] = content;
-        
-       return content;
+
+        return content;
     } else {
         // kan ikke lese
         return std::nullopt;
