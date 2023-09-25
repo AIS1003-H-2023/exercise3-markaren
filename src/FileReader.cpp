75fe3d933bd705038ddd1d05ed62fe545087a8ee
Lars Ivar Hatledal <laht@ntnu.no>
2023-09-25 12:45:59 +0200

    implemented solution

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -1,12 +1,23 @@
 
 #include "FileReader.hpp"
 
+#include <fstream>
+
 std::optional<std::string> FileReader::read(const std::filesystem::path &path) {
-
-    // Remember that the path argument is not necessary valid.
-    // The path may not exist due to user error (non-existing path) or system changes (file was moved/deleted).
-
-    // Note: you need to preserve newlines when you read the file into memory
-
-    return std::nullopt;//placeholder
+    
+    std::ifstream file(path);
+    
+    if (file.is_open()) {
+        
+        std::string content;
+        std::string line;
+        while(std::getline(file, line)) {
+            content += line + "\n";
+        }
+        
+       return content;
+    } else {
+        // kan ikke lese
+        return std::nullopt;
+    }
 }
